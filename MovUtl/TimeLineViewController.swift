76b300b2d1d16f08befc5565c9173677281b2389
RkEclair <ryosukadnak@gmail.com>
2017-06-10 15:59:54 +0900

    Add ruler

--- MovUtl/TimeLineViewController.swift
+++ MovUtl/TimeLineViewController.swift
@@ -4,16 +4,20 @@ class TimeLineViewController : NSViewController {
     @IBOutlet var sceneMenu: NSPopUpButton!
     @IBOutlet var scaleLevel: NSLevelIndicator!
     
-    @IBOutlet var layerScrollView: NSScrollView!
+    @IBOutlet var layerScrollView: NSScrollView! {
+        didSet {
+            layerScrollView.hasHorizontalRuler = true
+            layerScrollView.rulersVisible = true
+        }
+    }
     @IBOutlet var layerHeadScrollView: NSScrollView!
-    @IBOutlet var layerScaleScrollView: NSScrollView!
     
     override func awakeFromNib() {
+        NSScrollView.setRulerViewClass(TimeLineRulerView.classForCoder())
         NotificationCenter.default.addObserver(self, selector: #selector(scrolled), name: NSNotification.Name.NSScrollViewWillStartLiveScroll, object: nil)
     }
     
     func scrolled() {
         layerHeadScrollView.scroll(NSPoint(x: 0, y: layerScrollView.contentSize.height))
-        layerScaleScrollView.scroll(NSPoint(x: layerScaleScrollView.contentSize.width, y: 0))
     }
 }
