c393c10f47f55b79b45f656e3327fadd70edac6d
RkEclair <ryosukadnak@gmail.com>
2017-06-11 01:32:55 +0900

    Combaine views

--- MovUtl/TimeLineViewController.swift
+++ MovUtl/TimeLineViewController.swift
@@ -29,11 +29,8 @@ class TimeLineViewController : NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let document = self.view.window?.windowController?.document as! Document
-        document.data.layers.append(LayerData())
-        document.data.layers[0].objects?.append(TimeLineObject())
-        document.data.layers[0].objects?[0].endFrame = 150
-        layerScrollStackView.addArrangedSubview(TimeLineLayerView(data: document.data.layers[0]))
+//        
+//        layerScrollStackView.addArrangedSubview(TimeLineLayerView(data: document.data.layers[0]))
     }
     
     @IBAction func addScene(_ sender: NSMenuItem) {
--- MovUtl/ViewController.swift
+++ MovUtl/ViewController.swift
@@ -1,11 +1,19 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet var editView: EditView!
+    @IBOutlet var timeLineView: NSView!
+    @IBOutlet var propertyView: NSView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        childViewControllers.forEach { vc in
+            print(vc)
+        }
+    }
+    
     func updateDocument(with doc: Document) {
         
     }
-    @IBOutlet var editContainerView: NSView!
-    @IBOutlet var timeLineContainerView: NSView!
-    @IBOutlet var propertyContainerView: NSView!
 }
 
